e144c1dff00572d8267f315704ea1782ec6cc40a
Christian Alvarez <christian@christian-Aspire-5536.(none)>
2015-01-08 22:42:21 +0100

    Terminada pasado pep8

--- uaclient.py
+++ uaclient.py
@@ -9,9 +9,11 @@ import time
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
-def WriteLog (Mensaje):
+
+def WriteLog(Mensaje):
     hora = time.strftime('%Y%m%d%H%M%S', time.gmtime(time.time()))
     log.write(hora + " " + Mensaje + "\r\n")
+
 
 class SmallSMILHandler(ContentHandler):
 
@@ -60,18 +62,18 @@ class SmallSMILHandler(ContentHandler):
         return self.tags
 
 if __name__ == "__main__":
-	# Comprobación de la linea de argumentos
+    # Comprobación de la linea de argumentos
     method_list = ['Register', 'Invite', 'Bye']
     if len(sys.argv) != 4:
         sys.exit('Usage: python uaclient.py config method option')
-      
+
     method = sys.argv[2][0].upper() + sys.argv[2][1:].lower()
     if not method in method_list:
         sys.exit('Usage: python uaclient.py config method option')
 
     if not os.path.exists(sys.argv[1]):
         sys.exit("El archivo " + sys.argv[1] + " no existe")
-   
+
     # Obtención de datos del fichero xml
     fich = open(sys.argv[1])
     parser = make_parser()
@@ -88,7 +90,7 @@ if __name__ == "__main__":
     if not SERVER_IP:
         SERVER_IP = '127.0.0.1'
     SERVER_PORT = int(list_tags[3]['puerto'])
-   
+
     # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
     my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -96,7 +98,7 @@ if __name__ == "__main__":
 
     #Envio (La variable method la hemos conseguido al principio)
     if method == 'Register':
-        LINE = method + " sip:" + list_tags[0]['username'] + ':' 
+        LINE = method + " sip:" + list_tags[0]['username'] + ':'
         LINE += list_tags[1]['puerto'] + ' SIP/2.0\r\nExpires: ' + sys.argv[3]
         LINE += '\r\n\r\n'
     elif method == 'Invite':
@@ -104,13 +106,13 @@ if __name__ == "__main__":
             list_tags[1]['ip'] = '127.0.0.1'
         LINE = method + ' sip:' + sys.argv[3] + ' SIP/2.0\r\n'
         LINE += 'Content-Type: application/sdp\r\n\r\n'
-        LINE += 'v=0\r\no=' + list_tags[0]['username'] + ' ' 
+        LINE += 'v=0\r\no=' + list_tags[0]['username'] + ' '
         LINE += list_tags[1]['ip']
         LINE += '\r\ns=misesion\r\nt=0\r\nm=audio ' + list_tags[2]['puerto']
         LINE += ' RTP\r\n\r\n'
     elif method == 'Bye':
         LINE = method + ' sip:' + sys.argv[3] + ' SIP/2.0\r\n\r\n'
-      
+
     try:
         print "Enviando: " + LINE
         mens = LINE.split("\r\n")
@@ -118,7 +120,7 @@ if __name__ == "__main__":
         aLog += mens[0]
         WriteLog(aLog)
         my_socket.send(LINE)
-        data = my_socket.recv(1024) #RECIBO
+        data = my_socket.recv(1024)  # RECIBO
         print 'Recibido -- ', data
         data_list = data.split(" ")
     except socket.error:
@@ -148,7 +150,11 @@ if __name__ == "__main__":
     else:
         mens = data.split("\r\n")
         aLog = "Received from " + SERVER_IP + ":" + str(SERVER_PORT)
-        aLog += ": " +  mens[0]
+        aLog += ": " + mens[0]
+        WriteLog(aLog)
+
+    if method == "Bye":
+        aLog = "Finishing."
         WriteLog(aLog)
 
     # Cerramos todo
